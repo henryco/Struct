4e83f4d9a5dc9346058a0f8b4e84406c4358598d
HenryCo <hd2files@gmail.com>
2016-09-16 00:12:47 +0200

    Struct
    Syntax analyzer updated, add links and arrays

--- src/parser/drivers/InputDriver.java
+++ src/parser/drivers/InputDriver.java
@@ -5,6 +5,7 @@ import container.StructNode;
 import java.io.BufferedReader;
 import java.io.FileReader;
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.Collections;
 import java.util.List;
 
@@ -14,11 +15,14 @@ import java.util.List;
  */
 public class InputDriver {
 
-    private String[] ARRAY_TYPES = new String[]{"[", "]", "(", ")", "<", ">"};
+    private String[] ARRAY_TYPES = new String[]{"[", "]", "(", ")"};
     private String[] COMMENT_TYPES = new String[]{"//"};
     private String[] STRING_TYPES = new String[]{"\'", "\""};
     private String[] EQUALS_TYPES = new String[]{"="};
     private String[] IGNORED_TYPES = new String[]{";"};
+    private String[] SPLIT_TYPES = new String[]{":", " ", "->", ">>"};
+
+    private boolean includeTxt = true;
 
     InputDriver() {
 
@@ -33,7 +37,7 @@ public class InputDriver {
         List<String[]> headerList = new ArrayList<>();
         String textLine;
         long time0;
-        try{
+        try {
             BufferedReader br = new BufferedReader(new FileReader(url));
             time0 = System.nanoTime();
 
@@ -42,52 +46,51 @@ public class InputDriver {
                 String trimmedLine = textLine.trim();
 
                 if (checkImports(trimmedLine)) {
-                    String[] headers = splitLine(trimmedLine, new String[0], EQUALS_TYPES, IGNORED_TYPES);
+                    String[] headers = splitLine(trimmedLine, new String[0], EQUALS_TYPES, IGNORED_TYPES, SPLIT_TYPES);
 
                     for (int i = 0; i < headers.length; i++)
                         headers[i] = headers[i].trim();
                     headerList.add(headers);
-                }
-                else if (checkBody(trimmedLine, COMMENT_TYPES[0])) {
+                } else if (checkBody(trimmedLine, COMMENT_TYPES[0])) {
 
                     tokeLine = splitLine(trimmedLine, createMultiArray(ARRAY_TYPES, STRING_TYPES),
-                            EQUALS_TYPES, IGNORED_TYPES);
+                            EQUALS_TYPES, IGNORED_TYPES, SPLIT_TYPES);
 
                     for (int i = 0; i < tokeLine.length; i++)
-                        tokeLine[i] = tokeBodyLine(tokeLine[i], ARRAY_TYPES, COMMENT_TYPES, STRING_TYPES);
+                        tokeLine[i] = tokeBodyLine(tokeLine[i], ARRAY_TYPES, COMMENT_TYPES, STRING_TYPES, includeTxt);
                     bodyList.add(tokeLine);
                 }
             }
-            bodyList = prepareBrackets(bodyList, ARRAY_TYPES, STRING_TYPES);
+            bodyList = prepareBrackets(bodyList, ARRAY_TYPES, STRING_TYPES, includeTxt);
 
             ArrayList<String[]> tmp = new ArrayList<>();
-            bodyList.forEach( b -> {if (b.length > 0) tmp.add(b);});
+            bodyList.forEach(b -> {
+                if (b.length > 0) tmp.add(b);
+            });
             bodyList = tmp;
             bodyList = new ArrayList<>(prepareDots(bodyList));
 
             long time1 = System.nanoTime() - time0;
-            String finish = "Compiled INPUT_DRIVER: "+time1+" ns";
+            String finish = "Compiled INPUT_DRIVER: " + time1 + " ns";
             printUnderlined(finish, "#");
 
-        }catch(Exception e){
+        } catch (Exception e) {
             e.printStackTrace();
         }
 
 
-
+        @SuppressWarnings("unchecked")
         List<String[]>[] returnListArray = new ArrayList[2];
         returnListArray[0] = headerList;
         returnListArray[1] = bodyList;
         return returnListArray;
     }
 
-    private static void printUnderlined(String msg, String sym){
+    private static void printUnderlined(String msg, String sym) {
         System.out.println(msg);
         for (int i = 0; i < msg.length(); i++) System.out.print(sym);
         System.out.println("");
     }
-
-
 
 
     private static boolean needRefactor(List<String[]> list) {
@@ -96,53 +99,71 @@ public class InputDriver {
     }
 
 
-
-
-    private static String[] splitLine(String bodyLine, String[] exceptions, String[] eqSymbols, String[] ignored) {
+    private static String[] splitLine(String bodyLine, String[] exceptions, String[] eqSymbols, String[] ignored,
+                                      String[] split) {
         String newBodyLine = replaceFor(bodyLine, eqSymbols, ":");
         newBodyLine = replaceFor(newBodyLine, ignored, " ");
-
-        String[] newLine = newBodyLine.split(":");
         ArrayList<String> lineList = new ArrayList<>();
-        for (String line : newLine) {
-            line = line.trim();
-            int ex = 0;
-            String extraSym = "";
-            boolean extra = false;
-            for (String s : exceptions) {
-                if (line.startsWith(s)) {
-                    ex += 1;
+        newBodyLine = newBodyLine.trim();
+        int ex = 0;
+        String extraSym = "";
+        boolean extra = false;
+
+        for (String s : exceptions) {
+            if (newBodyLine.startsWith(s)) {
+                ex += 1;
+                break;
+            } else if (newBodyLine.contains(s)) extra = true;
+        }
+        if (extra) {
+            int firstSym = 0;
+            char[] charArray = newBodyLine.toCharArray();
+            for (int f = 0; f < charArray.length; f++) {
+                if (checkCharMatch(charArray[f], exceptions)) {
+                    firstSym = f;
                     break;
-                } else if (line.contains(s)) extra = true;
-            }
-            if (extra) {
-                int firstSym = 0;
-                char[] charArray = line.toCharArray();
-                for (int f = 0; f < charArray.length; f++) {
-                    if (checkCharMatch(charArray[f], exceptions)) {
-                        firstSym = f;
-                        break;
-                    }
                 }
-                String firSt = line.substring(0, firstSym).trim();
-                String secSt = line.substring(firstSym);
-                line = firSt;
-                extraSym = secSt;
             }
-            if (ex > 0) lineList.add(line);
-            else {
-                String[] listLine = line.split(" ");
-                for (String l : listLine) if (!l.isEmpty()) lineList.add(l);
-            }
-            if (extra) lineList.add(extraSym);
-        }return lineList.toArray(new String[lineList.size()]);
+            String firSt = newBodyLine.substring(0, firstSym).trim();
+            String secSt = newBodyLine.substring(firstSym);
+            newBodyLine = firSt;
+            extraSym = secSt;
+        }
+        if (ex > 0) lineList.add(newBodyLine);
+        else {
+            String[] listLine = tokeSplit(newBodyLine, split);
+            for (String l : listLine) if (!l.isEmpty()) lineList.add(l);
+        }
+        if (extra) lineList.add(extraSym);
+        return lineList.toArray(new String[lineList.size()]);
     }
 
 
+    private static String[] tokeSplit(String line, String[] seq) {
+
+        List<String> tmpList = new ArrayList<>();
+        tmpList.add(line);
+        List<String> strList = tokeSeq(tmpList, seq);
+
+        return strList.toArray(new String[strList.size()]);
+    }
+
+    private static List<String> tokeSeq(List<String> strList, String[] sec) {
+
+        List<String> srcList = new ArrayList<>(strList);
+        for (String aSec : sec) {
+            List<String> tmp = new ArrayList<>();
+            for (String aSrcList : srcList) {
+                String[] split = aSrcList.split(aSec);
+                Arrays.stream(split).forEach(tmp::add);
+            }
+            srcList = new ArrayList<>(tmp);
+        }
+        return srcList;
+    }
 
 
-
-    private static String tokeBodyLine(String toke, String[] arrType, String[] commentsType, String[] txtType) {
+    private static String tokeBodyLine(String toke, String[] arrType, String[] commentsType, String[] txtType, boolean includeTxt) {
 
         for (String aCommentsType : commentsType)
             if (toke.contains(aCommentsType)) {
@@ -150,58 +171,56 @@ public class InputDriver {
                 break;
             }
         for (int z = 0; z < arrType.length - 1; z += 2)
-            if (toke.startsWith(arrType[z]) && toke.endsWith(arrType[z+1])) return toke.trim();
+            if (toke.startsWith(arrType[z]) && toke.endsWith(arrType[z + 1])) return toke.trim();
         int[] start_end = new int[2];
         int actual = 0;
         char[] arr = toke.toCharArray();
-        for (int i = 0; i < arr.length; i++)
-            for (String tx : txtType) {
-                if (arr[i] == tx.toCharArray()[0]){
-                    start_end[actual] = i;
-                    actual++;
-                    break;
-                }
-            }
+        if (!includeTxt)
+            for (int i = 0; i < arr.length; i++)
+                for (String tx : txtType)
+                    if (arr[i] == tx.toCharArray()[0]) {
+                        start_end[actual] = i;
+                        actual++;
+                        break;
+                    }
         if (actual >= 2) return toke.substring((start_end[0] + 1), start_end[1]);
         return toke.trim();
     }
 
 
-
-
-
-    private static String[] prepareArrays(String[] arr, String[] arrTypes, String[] txtTypes) {
+    private static String[] prepareArrays(String[] arr, String[] arrTypes, String[] txtTypes, boolean includeTXT) {
 
         for (int i = 0; i < arr.length; i++)
             for (int z = 0; z < arrTypes.length - 1; z += 2)
-                if (arr[i].startsWith(arrTypes[z]) && arr[i].endsWith(arrTypes[z+1])) {
+                if (arr[i].startsWith(arrTypes[z]) && arr[i].endsWith(arrTypes[z + 1])) {
                     StringBuilder buffer = new StringBuilder();
                     char[] charArray = arr[i].toCharArray();
                     boolean open = false;
                     for (char ch : charArray) {
-                        if (checkCharMatch(ch, txtTypes)) open = !open;
+                        if (checkCharMatch(ch, txtTypes)) {
+                            open = !open;
+                            if (includeTXT) buffer.append(ch);
+                        }
                         if (!open && ch != ' ' && !checkCharMatch(ch, txtTypes)) buffer.append(ch);
                         else if (!checkCharMatch(ch, txtTypes) && open) buffer.append(ch);
-                    }   arr[i] = buffer.toString();
+                    }
+                    arr[i] = buffer.toString();
                     break;
                 }
         return arr;
     }
 
 
-
-
-
     private static List<String[]> prepareBrackets(List<String[]> dataList, String[] arrType,
-                                                  String[] txtType) {
+                                                  String[] txtType, boolean includeTXT) {
 
         int bracketsForRemove = 0;
         int bracketsOpen = 0;
         int iterationsNeed = dataList.size();
         for (int i = 0; i < iterationsNeed; i++) {
 
-            String[] arr = prepareArrays(dataList.get(i), arrType, txtType);
-            if(arr[0].contains("}")) {
+            String[] arr = prepareArrays(dataList.get(i), arrType, txtType, includeTXT);
+            if (arr[0].contains("}")) {
 
                 bracketsOpen -= 1;
                 if (bracketsForRemove > 0 && bracketsOpen == bracketsForRemove) {
@@ -209,34 +228,31 @@ public class InputDriver {
                     bracketsForRemove -= 1;
                 }
             }
-            if ((!arr[0].contains("{") && !arr[0].contains("}")) && (arr.length < 2 || arr[arr.length-1].isEmpty())) {
+            if ((!arr[0].contains("{") && !arr[0].contains("}")) && (arr.length < 2 || arr[arr.length - 1].isEmpty())) {
 
                 i += 1;
-                if(dataList.get(i)[0].contains("{")) {
+                if (dataList.get(i)[0].contains("{")) {
 
-                    dataList.set(i-1, new String[]{arr[0], "{"});
+                    dataList.set(i - 1, new String[]{arr[0], "{"});
                     dataList.set(i, new String[0]);
                     bracketsOpen += 1;
                 }
-            }
-            else if (!arr[0].contains("{") && arr.length >= 2 && arr[arr.length-1].contains("{")) {
+            } else if (!arr[0].contains("{") && arr.length >= 2 && arr[arr.length - 1].contains("{")) {
                 bracketsOpen += 1;
             }
-            if (arr[0].contains("{") && i > 0 && dataList.get(i-1).length >= 2
-                    && !dataList.get(i-1)[1].isEmpty()) {
+            if (arr[0].contains("{") && i > 0 && dataList.get(i - 1).length >= 2
+                    && !dataList.get(i - 1)[1].isEmpty()) {
 
                 bracketsForRemove += 1;
                 bracketsOpen += 1;
                 dataList.set(i, new String[0]);
-            }
-            else if (arr[0].contains("{") && i > 0 && (dataList.get(i-1).length == 0
-                    || dataList.get(i-1)[0].isEmpty() || dataList.get(i-1)[0].contains("}"))) {
+            } else if (arr[0].contains("{") && i > 0 && (dataList.get(i - 1).length == 0
+                    || dataList.get(i - 1)[0].isEmpty() || dataList.get(i - 1)[0].contains("}"))) {
 
                 bracketsForRemove += 1;
                 bracketsOpen += 1;
                 dataList.set(i, new String[0]);
-            }
-            else if (arr[0].contains("{") && i == 0) {
+            } else if (arr[0].contains("{") && i == 0) {
 
                 dataList.set(0, new String[0]);
                 dataList.set((dataList.size() - 1), new String[0]);
@@ -245,9 +261,6 @@ public class InputDriver {
         }
         return dataList;
     }
-
-
-
 
 
     private static List<String[]> prepareDots(List<String[]> dataList) {
@@ -269,8 +282,8 @@ public class InputDriver {
                         closure.add(new String[]{"}"});
                 else {
                     int otherClosures = 1;
-                    int j = i+1;
-                    while(otherClosures > 0){
+                    int j = i + 1;
+                    while (otherClosures > 0) {
                         String[] nextLine = dataList.get(j);
                         for (String aNextLine : nextLine) {
                             if (aNextLine.contains("{") && nextLine.length > 1) otherClosures += 1;
@@ -285,7 +298,7 @@ public class InputDriver {
                 for (int p = forDelete; p < dataList.size(); p++)
                     closure.add(dataList.get(p));
                 break;
-            }   else closure.add(arr);
+            } else closure.add(arr);
         }
         /* Possible may need hard clone List */
         if (needRefactor(closure)) return prepareDots(closure);
@@ -293,13 +306,10 @@ public class InputDriver {
     }
 
 
-
-
     private static boolean checkCharMatch(char ch, String[] type) {
         for (String tp : type) if (ch == tp.toCharArray()[0]) return true;
         return false;
     }
-
 
 
     private static String replaceFor(String line, String[] symbols, String replaceSym) {
@@ -334,42 +344,59 @@ public class InputDriver {
     }
 
 
-
-
     protected String[] getARRAY_TYPES() {
         return ARRAY_TYPES;
     }
+
     protected InputDriver setARRAY_TYPES(String[] ARRAY_TYPES) {
         this.ARRAY_TYPES = ARRAY_TYPES;
         return this;
     }
+
     protected String[] getCOMMENT_TYPES() {
         return COMMENT_TYPES;
     }
+
     protected InputDriver setCOMMENT_TYPES(String[] COMMENT_TYPES) {
         this.COMMENT_TYPES = COMMENT_TYPES;
         return this;
     }
+
     protected String[] getSTRING_TYPES() {
         return STRING_TYPES;
     }
+
     protected InputDriver setSTRING_TYPES(String[] STRING_TYPES) {
         this.STRING_TYPES = STRING_TYPES;
         return this;
     }
+
     protected String[] getEQUALS_TYPES() {
         return EQUALS_TYPES;
     }
+
     protected InputDriver setEQUALS_TYPES(String[] EQUALS_TYPES) {
         this.EQUALS_TYPES = EQUALS_TYPES;
         return this;
     }
+
     protected String[] getIGNORED_TYPES() {
         return IGNORED_TYPES;
     }
+
     protected InputDriver setIGNORED_TYPES(String[] IGNORED_TYPES) {
         this.IGNORED_TYPES = IGNORED_TYPES;
         return this;
     }
+
+    protected boolean isIncludeTxt() {
+        return includeTxt;
+    }
+
+    protected InputDriver setIncludeTxt(boolean includeTxt) {
+        this.includeTxt = includeTxt;
+        return this;
+    }
+
 
 }
