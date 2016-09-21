39a8e0474d3998cac0f4ab8500430953fca41aab
HenryCo <hd2files@gmail.com>
2016-09-22 00:30:09 +0200

    Struct

--- src/StaticVoidMainEnter.java
+++ src/StaticVoidMainEnter.java
@@ -5,20 +5,17 @@ import net.henryco.struct.container.tree.StructTree;
 import java.util.Scanner;
 
 /**
+ * src/examples/example2.struct
  * @author Henry on 09/09/16.
  */
 public class StaticVoidMainEnter {
 
 
     public static void main(String[] args) {
-
-        /*
-            src/examples/example2.struct
-        */
-        StructTree container = StructContainer.tree(Struct.printDataFile(Struct.in.readStructData(inConsole(">> "))));
+        StructTree container = StructContainer.tree(Struct.printDataFile(Struct.in.readStructData(fromConsole(">> "))));
     }
 
-    public static String inConsole(String inMsg) {
+    public static String fromConsole(String inMsg) {
         System.out.print(inMsg);
         Scanner in = new Scanner(System.in);
         String url = in.next();
